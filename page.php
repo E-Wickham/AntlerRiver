a7941a9802906eec8d4b1c72bb8328a6040f32eb
E-Wickham <ericwickham@hotmail.com>
2024-09-10 10:40:22 -0400

    default page styling

--- page.php
+++ page.php
@@ -1,22 +1,22 @@
 <?php 
 /**
- * Template for all Pages on Farma website
+ * Template for all Pages
  */
 get_header();
 ?>
+
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
         <?php
-        while ( have_posts() ) :
-            the_post();
-            get_template_part( 'template-parts/page/content', 'page'); // content-page.php
-        
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
-        endwhile;
+            while ( have_posts() ) :
+                the_post();
+                get_template_part( 'template-parts/page/content', 'page'); // content-page.php
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
+            endwhile;
         ?>
     </main>
 </div>
-<?php get_footer();
+    <?php get_footer();
 ?>
